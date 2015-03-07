153f31a36efcc1591f6ed21abcdb7edfe3c8ab07
Jonathan Parrish <yoda.peh@gmail.com>
2015-03-07 12:33:06 -0500

    Change response total for Video

--- app/controllers/api/v1/joins_controller.rb
+++ app/controllers/api/v1/joins_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::JoinsController < ApplicationController
 
 
   def index
-    response = [Echonest.new("beatles").video, Setlist.new("beatles").sets]
+    artist_name = "beatles"
+    response = [Echonest.new(artist_name).video, Setlist.new(artist_name).sets]
     render json: response
   end
 
--- app/models/echonest.rb
+++ app/models/echonest.rb
@@ -4,7 +4,7 @@ class Echonest
   def initialize(artist_name)
     key = "api_key=#{ENV['ECHONEST_KEY']}"
 
-    @response = HTTParty.get("http://developer.echonest.com/api/v4/artist/video?#{key}&name=#{artist_name}&format=json&results=5&start=0")
+    @response = HTTParty.get("http://developer.echonest.com/api/v4/artist/video?#{key}&name=#{artist_name}&format=json&results=1&start=0")
   end
 
 
